7ea5cc2649db1a2cbedee2dcad79954708416b4f
Azis Abdul Bachar <azis.abdul.bachar@gmail.com>
2017-03-29 11:03:48 +0700

    complete tryout 1

--- TryOut-1/tryout2.js
+++ TryOut-1/tryout2.js
@@ -1,10 +1,16 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var DOMParser = require("dom-parser");
-var parser = new DOMParser();
-var xhr = new XMLHttpRequest();
-xhr.open( "GET", process.argv[2], false ); // false for synchronous request
-xhr.send( null );
-var data = xhr.responseText;
-var xmlDoc=parser.parseFromString(data,"text/xml");
-var title = xmlDoc.getElementsByTagName("title");
-console.log(title);
+var request = require('request');
+
+request(process.argv[2], function(error, response, body) {
+    var html = (response.headers['content-type'].includes('text/html')) ? ' ' : ' not ';
+    console.log('The result is' + html + 'HTML');
+    console.log('The title is ' + getTitle(body));
+});
+
+function getTitle(body) {
+    var reg = /(<\s*title[^>]*>(.+?)<\s*\/\s*title)>/gi;
+    var str = body.toString();
+    var match = reg.exec(str);
+    if (match && match[2]) {
+        return match[2];
+    }
+}
